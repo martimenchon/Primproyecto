d81b5b7e0d080bcdaeba8b0a5658f227a6bdd25b
martimenchon <martumenchon@gmail.com>
2024-04-21 22:06:38 -0300

    primera modificacion para el primer desafio

---
+++ js/main.js
@@ -0,0 +1,26 @@
+let nombreProducto = "mesa";
+let precioUnitario = 20000;
+let cantidadDeseada = prompt("Ingrese la cantidad de producto que desea comprar")
+
+let costoTotal = precioUnitario * cantidadDeseada
+
+let alerta = `El costo total de su compra es: ${costoTotal}`;
+
+alert(alerta)
+
+
+
+
+//Creá 3 variables:
+
+//Una para asignar el nombre del producto (nombreProducto)
+
+//Una para el precio por unidad (precioUnitario) con sus respectivos valores 
+
+//Y otra que almacenará la cantidad deseada por el usuario (cantidadDeseada) 
+
+//Creá un prompt donde se le pregunte al usuario la cantidad que desea comprar y que esa respuesta se almacene en cantidadDeseada.
+
+//Luego calcular el costo total de la compra multiplicando el precioUnitario por la cantidadDeseada
+
+//Utilizá alert() para informar al usuario sobre el costo total de su compra 
